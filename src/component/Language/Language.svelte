f3e76f899a4f39a55e7c0d0dc81ea8fe77ada1b8
thaiqt0002 <thaiqt0002@gmail.com>
2023-10-24 23:00:13 +0700

    fast fix progress bar

--- src/component/Language/Language.svelte
+++ src/component/Language/Language.svelte
@@ -6,31 +6,31 @@
     <div class="skill-box">
         <span class="title">Svelte</span>
         <div class="skill-bar">
-            <span class="skill-per bg-red-500" style='width:41.5%'>
-                <span class="tooltip">41.5%</span>
+            <span class="skill-per bg-red-500" style='width:48.8%'>
+                <span class="tooltip">48.8%</span>
             </span>
         </div>
     </div>
     <div class="skill-box">
         <span class="title">Javascript</span>
         <div class="skill-bar">
-            <span class="skill-per" style='animation-delay: 0.1s; width:29.1%'>
-                <span class="tooltip">29.1%</span>
+            <span class="skill-per" style='animation-delay: 0.1s; width:28%'>
+                <span class="tooltip">28.0%</span>
             </span>
         </div>
     </div>
     <div class="skill-box">
         <span class="title">CSS</span>
         <div class="skill-bar">
-            <span class="skill-per javascript red" style='animation-delay: 0.2s; width:25.5%'>
-                <span class="tooltip">25.5%</span>
+            <span class="skill-per javascript red" style='animation-delay: 0.2s; width:20.5%'>
+                <span class="tooltip">20.5%</span>
             </span>
         </div>
     </div>
     <div class="skill-box">
         <span class="title">HTML</span>
         <div class="skill-bar">
-            <span class="skill-per" style='animation-delay: 0.3s; width:3.9%'>
+            <span class="skill-per" style='animation-delay: 0.3s; width:10%'>
                 <span class="tooltip">&lt;10%</span>
             </span>
         </div>
