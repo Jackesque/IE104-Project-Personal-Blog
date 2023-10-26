4ba44cb33ed8bacb28ce41e98f952c35e1e0b3a1
thaiqt0002 <thaiqt0002@gmail.com>
2023-10-26 15:01:04 +0700

    fix path configjs

--- svelte.config.js
+++ svelte.config.js
@@ -11,8 +11,10 @@ const config = {
 		adapter: adapter(),
 
 	},
-
+	
     preprocess: [vitePreprocess({})]
 }
+
+config.paths = { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
 
 export default config;
