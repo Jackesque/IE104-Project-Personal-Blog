b4c9be91c8bf0fcb23bc344c7550d2502e17d314
jackesque <127185476+Jackesque@users.noreply.github.com>
2023-10-26 09:30:11 +0700

    Updates

---
+++ _app/immutable/nodes/5.daeb1d6c.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.e108d1fd.js";import{S as t,i as e}from"../chunks/index.bad58c3a.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
