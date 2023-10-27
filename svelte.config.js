1f8830684a230860e3987fed6f3f632f1f2e3552
jackesque <127185476+Jackesque@users.noreply.github.com>
2023-10-27 17:47:19 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,10 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
   
   kit: {
+    paths: {
+      base: 'IE104-Project-Personal-Blog',
+    },
+    
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
