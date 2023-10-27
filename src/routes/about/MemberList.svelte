57bc496201d1c9f4fcfad5aabda56dcc7c682999
jackesque <127185476+Jackesque@users.noreply.github.com>
2023-10-27 15:58:02 +0700

    add Thai Dao img

--- src/routes/about/MemberList.svelte
+++ src/routes/about/MemberList.svelte
@@ -17,7 +17,7 @@
             <h2 class='text-xl'>MSSV:</h2>
         </div>
         <div class='flex flex-col items-center'>
-            <img class='w-80 m-3 rounded-md' src='' alt='Thai2'/>
+            <img class='w-80 m-3 rounded-md' src='{base}/images/ava3.jpg' alt='Thai2'/>
             <h1 class='text-2xl font-medium m-1'>Đào Duy Thái</h1>
             <h2 class='text-xl'>MSSV:</h2>
         </div>
