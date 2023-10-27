d656ae58195088ad052f6ef342b2100acf8cf507
jackesque <127185476+Jackesque@users.noreply.github.com>
2023-10-27 16:59:24 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,26 +3,29 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-
-    kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
+  
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter({
       
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
-	},
-	
-    preprocess: [vitePreprocess({})]
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true
+    }),
+  },
+  paths: {
+    base: '/IE104-Project-Personal-Blog',
+    assets: '/IE104-Project-Personal-Blog'
+  },
+  preprocess: [vitePreprocess({})]
 }
 
-config.paths = { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
+// config.paths = { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
 
 export default config;
