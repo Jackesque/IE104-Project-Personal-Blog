27612f0fb378783fe411ebe503ee47bd6276d7eb
jackesque <127185476+Jackesque@users.noreply.github.com>
2023-10-27 14:49:46 +0700

    Update +page.svelte

--- src/routes/cardDetails/2/+page.svelte
+++ src/routes/cardDetails/2/+page.svelte
@@ -1,18 +1,4 @@
 <script>
-<<<<<<< Updated upstream
-    import {base} from '$app/paths'  
-</script>
-
-<div class='w-[80%] flex flex-col'>
-    <div class='border-b-2 border-gray-600 pb-20'>
-        <img src='{base}/images/card2.png' alt=''/>
-        <h1 class='font-bold text-4xl pt-5'>Đây chỉ là ví dụ cho tiêu đề, vui lòng bỏ qua </h1>
-        <p class='pt-5'><i>Tác giả: Không quan trọng</i><br><br>
-        <i>Cập nhật lần cuối: 26/10/2023</i><br><br>
-        Nội dung: Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis vitae placeat soluta magni eius quia distinctio, reprehenderit minus, at culpa, quaerat praesentium dignissimos. Recusandae reiciendis iste, quisquam reprehenderit voluptate cupiditate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio eligendi autem voluptatem aliquam officia odit, sint, obcaecati aliquid totam, modi deleniti. Iure delectus impedit excepturi ut id consequatur saepe alias? <br><br>
-        </p>
-    </div>
-=======
   import { base } from '$app/paths'
   import card2 from '$lib/images/card2.png'
 </script>
@@ -26,5 +12,4 @@
       Nội dung: Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis vitae placeat soluta magni eius quia distinctio, reprehenderit minus, at culpa, quaerat praesentium dignissimos. Recusandae reiciendis iste, quisquam reprehenderit voluptate cupiditate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio eligendi autem voluptatem aliquam officia odit, sint, obcaecati aliquid totam, modi deleniti. Iure delectus impedit excepturi ut id consequatur saepe alias? <br><br>
     </p>
   </div>
->>>>>>> Stashed changes
 </div>
