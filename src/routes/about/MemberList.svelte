3ae4ba820c1189390bd5a20cb2b34232cf08598e
duythai03 <21522578@gm.uit.edu.vn>
2023-10-28 14:39:25 +0700

    add mssv in about

--- src/routes/about/MemberList.svelte
+++ src/routes/about/MemberList.svelte
@@ -9,17 +9,17 @@
         <div class='flex flex-col items-center'>
             <img class='w-80 m-3 rounded-md' src='{base}/images/ava1.png' alt='Thai1'/>
             <h1 class='text-2xl font-medium m-1'>Trần Duy Thái</h1>
-            <h2 class='text-xl'>MSSV:21522582</h2>
+            <h2 class='text-xl'>MSSV: 21522582</h2>
         </div>
         <div class='flex flex-col items-center'>
             <img class='w-80 m-3 rounded-md' src='{base}/images/ava2.jpg' alt='Tien'/>
             <h1 class='text-2xl font-medium m-1'>Trần Quang Tiến</h1>
-            <h2 class='text-xl'>MSSV:</h2>
+            <h2 class='text-xl'>MSSV: 21522673</h2>
         </div>
         <div class='flex flex-col items-center'>
             <img class='w-80 m-3 rounded-md' src='{base}/images/ava3.jpg' alt='Thai2'/>
             <h1 class='text-2xl font-medium m-1'>Đào Duy Thái</h1>
-            <h2 class='text-xl'>MSSV:</h2>
+            <h2 class='text-xl'>MSSV: 21522578</h2>
         </div>
     </div>
 </div>
