386ab683119b8b69e518293c735083cbc9a1f6d2
jackesque <127185476+Jackesque@users.noreply.github.com>
2023-10-29 20:33:10 +0700

    change path to make css works

--- src/component/Navbar/Navbar.svelte
+++ src/component/Navbar/Navbar.svelte
@@ -9,10 +9,10 @@
 		<div class='flex justify-center pt-5 bg-black pb-1'>
 			<div class='flex justify-between w-4/5'>
 				<ul class='flex gap-8 text-base font-medium'>
-					<li class='pt-2' aria-current={$page.url.pathname === '/' ? '1' : undefined}><a href='{base}/'><span>Home</span></a></li>
-					<li class='pt-2' aria-current={$page.url.pathname === '/blog' ? '1' : undefined}><a href='{base}/blog'><span>Blog</span></a></li>
-					<li class='pt-2' aria-current={$page.url.pathname === '/feature' ? '1' : undefined}><a href='{base}/feature'><span>Feature</span></a></li>
-					<li class='pt-2' aria-current={$page.url.pathname === '/about' ? '1' : undefined}><a href='{base}/about'><span>About</span></a></li>
+					<li class='pt-2' aria-current={$page.url.pathname === `${base}/` ? '1' : undefined}><a href='{base}/'><span>Home</span></a></li>
+					<li class='pt-2' aria-current={$page.url.pathname === `${base}/blog/` ? '1' : undefined}><a href='{base}/blog'><span>Blog</span></a></li>
+					<li class='pt-2' aria-current={$page.url.pathname === `${base}/feature/` ? '1' : undefined}><a href='{base}/feature'><span>Feature</span></a></li>
+					<li class='pt-2' aria-current={$page.url.pathname === `${base}/about/` ? '1' : undefined}><a href='{base}/about'><span>About</span></a></li>
 
 				</ul>
 				
