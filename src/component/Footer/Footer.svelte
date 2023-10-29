180f6c78caddba135f260ede998b1af4a7bf6411
jackesque <127185476+Jackesque@users.noreply.github.com>
2023-10-29 21:03:44 +0700

    Update Footer.svelte

--- src/component/Footer/Footer.svelte
+++ src/component/Footer/Footer.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <div class='footer flex flex-wrap justify-center gap-x-8 gap-y-4 border-t-[1px] w-[80%] border-gray-300 p-5'>
-    <a href='https://github.com/Jackesque/IE104-Project-Personal-Blog' target="_blank">Mã nguồn</a>
-    <a href='{base}/design'><span>Thiết kế</span></a>
-    <div class='tooltip'>Bố cục</div>
-    <div class='tooltip'>Theo dõi</div>
-    <div class='tooltip'>Số liệu</div>
+    <a href='https://github.com/Jackesque/IE104-Project-Personal-Blog' target="_blank" class='tooltip'>Mã nguồn</a>
+    <a href='{base}/design' class='tooltip'><span>Thiết kế</span></a>
+    <a href="#" class='tooltip'>Bố cục</a>
+    <a href="#" class='tooltip'>Theo dõi</a>
+    <a href="#" class='tooltip'>Số liệu</a>
 </div>
 
 <style>
