a34b755ccd02707aa651fd2acfb316c1426ac5d9
jackesque <127185476+Jackesque@users.noreply.github.com>
2023-10-30 15:36:16 +0700

    remove pointless css

--- src/component/Footer/Footer.svelte
+++ src/component/Footer/Footer.svelte
@@ -3,15 +3,9 @@
 </script>
 
 <div class='footer flex flex-wrap justify-center gap-x-8 gap-y-4 border-t-[1px] w-[80%] border-gray-300 p-5'>
-    <a href='https://github.com/Jackesque/IE104-Project-Personal-Blog' target="_blank" class='tooltip'>Mã nguồn</a>
-    <a href='{base}/design' class='tooltip'><span>Thiết kế</span></a>
-    <a href="#" class='tooltip'>Bố cục</a>
-    <a href="#" class='tooltip'>Theo dõi</a>
-    <a href='{base}/statistic' class='tooltip'>Số liệu</a>
+    <a href='https://github.com/Jackesque/IE104-Project-Personal-Blog' target="_blank" class='hover:font-bold'>Mã nguồn</a>
+    <a href='{base}/design' class='hover:font-bold'><span>Thiết kế</span></a>
+    <a href="#" class='hover:font-bold'>Bố cục</a>
+    <a href="#" class='hover:font-bold'>Theo dõi</a>
+    <a href='{base}/statistics' class='hover:font-bold'>Số liệu</a>
 </div>
-
-<style>
-    .footer .tooltip:hover {
-        font-weight: bold;
-    }
-</style>
