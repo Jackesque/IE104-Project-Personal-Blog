2c56a5c70a5faad4d6a6f4e38f36efc50a63ecad
thaiqt0002 <thaiqt0002@gmail.com>
2023-11-01 17:37:13 +0700

    resize img

--- src/routes/about/MemberList.svelte
+++ src/routes/about/MemberList.svelte
@@ -6,7 +6,7 @@
     <span class='text-4xl font-bold pt-3 text-transparent bg-gradient-to-r 
     from-yellow-400 via-red-500 to-pink-500 bg-clip-text h-[4rem] mb-10 '>🔥Thành viên trong nhóm </span>
     <div class='grid md:grid-cols-3 gap-2 sm:grid-cols-1'>
-        <div class='flex flex-col items-center'>
+        <div class='flex flex-col items-center '>
             <img class='w-80 m-3 rounded-md' src='{base}/images/ava1.png' alt='Thai1'/>
             <h1 class='text-2xl font-medium m-1'>Trần Duy Thái</h1>
             <h2 class='text-xl'>MSSV: 21522582</h2>
