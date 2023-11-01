8398b1b3686051fcf03baf3ea6225ffed5a46096
duythai03 <21522578@gm.uit.edu.vn>
2023-11-01 15:03:20 +0700

    Update WebDesign.svelte
    update webdesign for themeswitch

--- src/routes/design/WebDesign.svelte
+++ src/routes/design/WebDesign.svelte
@@ -2,35 +2,39 @@
   <span class='text-4xl font-bold pt-3 text-transparent bg-gradient-to-r from-green-400 via-emerald-500 to-teal-400
   bg-clip-text h-[4rem] mb-10'>Thiết kế Website </span>
   <div class="mt-8 w-auto rounded p-4 border-2 border-dashed border-gray-600">
-    <p class="text-lg font-bold text-gray-300 mb-4 text-center">Font Family: Inter</p>
+    <div class="text-center">
+      <span class="text-2xl font-bold dark:hidden">Light Mode</span>
+      <span class="text-2xl font-bold hidden dark:block">Dark Mode</span>
+      <p class="mb-4 text-sm text-gray-600 dark:text-gray-300">Font Family: Inter</p>
+    </div>
     <div class="flex items-center flex-col gap-4" >
       <div class="colors-row-1 flex items-center gap-10">
         <div class="flex items-center gap-2">
           <div class="h-10 w-10 rounded shadow-none border border-gray-600 shrink-0 bg-white"></div>
           <div class="flex flex-col">
             <p>White Background</p>
-            <p class="text-sm text-gray-400">#ffffff</p>
+            <p class="text-sm text-gray-600 dark:text-gray-400">#ffffff</p>
           </div>
         </div>
         <div class="flex items-center gap-2">
           <div class="h-10 w-10 rounded shadow-none border border-gray-600 shrink-0 bg-black"></div>
           <div class="flex flex-col">
             <p>Dark Background</p>
-            <p class="text-sm text-gray-400">#0e1111</p>
+            <p class="text-sm text-gray-600 dark:text-gray-400">#0e1111</p>
           </div>
         </div>
         <div class="flex items-center gap-2">
           <div class="h-10 w-10 rounded shadow-none border border-gray-600 shrink-0 bg-lime-300"></div>
           <div class="flex flex-col">
             <p>Primary 200</p>
-            <p class="text-sm text-gray-400">#dbff00</p>
+            <p class="text-sm text-gray-600 dark:text-gray-400">#dbff00</p>
           </div>
         </div>
         <div class="flex items-center gap-2">
           <div class="h-10 w-10 rounded shadow-none border border-gray-600 shrink-0 bg-green-500"></div>
           <div class="flex flex-col">
             <p>Primary 300</p>
-            <p class="text-sm text-gray-400">#00e887</p>
+            <p class="text-sm text-gray-600 dark:text-gray-400">#00e887</p>
           </div>
         </div>
       </div>
@@ -40,25 +44,25 @@
           <div class="h-10 w-10 rounded shadow-none border border-gray-600 shrink-0 bg-cyan-400"></div>
           <div class="flex flex-col">
             <p>Primary 400</p>
-            <p class="text-sm text-gray-400">#00e0f3</p>
+            <p class="text-sm text-gray-600 dark:text-gray-400">#00e0f3</p>
           </div>
         </div>
         <div class="flex items-center gap-2">
           <div class="h-10 w-10 rounded shadow-none border border-gray-600 shrink-0 bg-cyan-600"></div>
           <div class="flex flex-col">
             <p>Primary 500</p>
-            <p class="text-sm text-gray-400">#00bfff</p>
+            <p class="text-sm text-gray-600 dark:text-gray-400">#00bfff</p>
           </div>
         </div>
         <div class="flex items-center gap-2">
           <div class="h-10 w-10 rounded shadow-none border border-gray-600 shrink-0 bg-gradient-to-r from-green-400 via-emerald-500 to-teal-400"></div>
           <div class="flex flex-col">
             <p>Gradient</p>
-            <p class="text-sm text-gray-400">#00e0f3 to #00bfff</p>
+            <p class="text-sm text-gray-600 dark:text-gray-400">#00e0f3 to #00bfff</p>
           </div>
         </div>
       </div>
     </div>
   </div>
-  
 </div>
+
